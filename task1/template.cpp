77ca8b73d69af9558273c3f3818de79b088c829b
Harsh Thorat <harsthorat848@gmail.com>
2026-04-05 16:33:27 +0530

    task2only

--- task1/template.cpp
+++ task1/template.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
+#include "convolution.hpp" 
 
 using namespace std;
 
@@ -23,12 +24,33 @@ int main()
         cerr << "Could not load image at ./assets/hogwarts.png\n";
         return -1;
     }
+     cv::Mat intermediate;
+      cv::Mat output_1;
+      cv::Mat output_2;
+
 
     cv::Mat img_f;
     img.convertTo(img_f, CV_64FC1);
+    intermediate = convolve(img, K1);
+    output_1 = convolve(intermediate,  K1); 
+
+    intermediate = convolve(img, K2);
+    output_2 = convolve(intermediate,  K2); 
+
+
+
+
+
+
+
 
     // TODO: apply K1 and K2 to img_f with as few multiplications per pixel as possible.
     // Print the number of multiplications your approach uses per pixel.
+     
+      cv::imshow("Output by Naive Seperable Convolution", output_1);
+      cv::waitKey(0); 
+      cv::imshow("Output by Naive Seperable Convolution", output_2);
+      cv::waitKey(0); 
 
     return 0;
 }
